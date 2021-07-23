9d2a85440f4a465585bf7b55a297fa6e83d85956
Thomas Meshail <toto.mm50.tm@gmail.com>
2021-07-23 10:00:33 +0200

    First Blog

---
+++ First Blog/script.js
@@ -0,0 +1,3 @@
+// Created by 2Math
+
+alert ("____________HELLO____________\n That is a trainning for creating a Web page by HTML CSS BOOTSTRAP so of coures all data is dum..... the web include gifs and photos but I couldn't upload them Enjoy!\n a project with 10 hours working\n i hope u like it" )
